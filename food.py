65f7d0fbed4524941dbc21d6ed66859037df3301
Shamase32 <137077799+Cyab1@users.noreply.github.com>
2024-09-03 20:26:38 +0200

    Initial commit

---
+++ food.py
@@ -0,0 +1,15 @@
+#create the variable to ask user for menu items
+#ask user to input the requered item using input
+#print out items in separate lines
+
+print("please order your 3 favorite food items:")
+# Ask the user for their 3 favorite food items on a menu
+item1 = input("Enter your first favorite food item: ")
+item2 = input("Enter your second favorite food item: ")
+item3 = input("Enter your third favorite food item: ")
+
+# Display the user's favorite food items
+print("Order confirmation! You have ordered:")
+print(item1)
+print(item2)
+print(item3)
